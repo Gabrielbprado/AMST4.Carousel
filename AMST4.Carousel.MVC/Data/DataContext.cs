f3588954e766827ee5c061eb540c1a93c9ae7184
Gabrielbprado <guardavege@gmail.com>
2024-04-22 18:13:39 -0300

    Added Data Context

---
+++ AMST4.Carousel.MVC/Data/DataContext.cs
@@ -0,0 +1,9 @@
+﻿using AMST4.Carousel.MVC.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AMST4.Carousel.MVC.Data;
+
+public class DataContext(DbContextOptions<DataContext> opts) : DbContext(opts)
+{
+    public DbSet<Category> Category { get; set; }
+}
--- AMST4.Carousel.MVC/Program.cs
+++ AMST4.Carousel.MVC/Program.cs
@@ -1,8 +1,15 @@
+using AMST4.Carousel.MVC.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<DataContext>(opts =>
+{
+    opts.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 
+});
 var app = builder.Build();
  
 // Configure the HTTP request pipeline.
