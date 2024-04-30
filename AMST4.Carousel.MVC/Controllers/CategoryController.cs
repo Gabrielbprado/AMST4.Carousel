a49e4a732495ef943ce5d2932b9b4b5c92bdefc2
Gabrielbprado <guardavege@gmail.com>
2024-04-30 08:11:06 -0300

    Add View Category List

---
+++ AMST4.Carousel.MVC/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using AMST4.Carousel.MVC.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AMST4.Carousel.MVC.Controllers;
+
+public class CategoryController : Controller
+{
+    private readonly DataContext _context;
+
+    public CategoryController(DataContext context)
+    {
+        _context = context;
+    }
+
+    public IActionResult CategoryList()
+    {
+        var categories = _context.Category.ToList();
+        return View(categories);
+    }
+}
